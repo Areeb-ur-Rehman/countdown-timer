23235de043e9dee3dfeb0718c9d66baeea090a6e
Areeb Ur Rehman <areebjutt026@gmail.com>
2023-12-26 02:23:55 +0500

    add

---
+++ script.js
@@ -0,0 +1,10 @@
+const newYears = '1st Jan 2024';
+
+function countdown () {
+    const newYearsDate = new Date(newYears);
+    const currentdate = new Date ();
+
+    console.log(newYearsDate - currentdate);
+}
+
+countdown();
